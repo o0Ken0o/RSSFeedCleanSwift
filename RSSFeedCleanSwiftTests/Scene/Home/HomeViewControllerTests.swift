308b2fe63de8b7908aa0e297ed6ee5ebd9dbd682
o0Ken0o <ken.siu@accedo.tv>
2018-02-20 12:31:54 +0800

    test all delegate methods of HomeViewController as well

--- RSSFeedCleanSwiftTests/Scene/Home/HomeViewControllerTests.swift
+++ RSSFeedCleanSwiftTests/Scene/Home/HomeViewControllerTests.swift
@@ -52,6 +52,65 @@ class HomeViewControllerTests: XCTestCase {
         XCTAssert(interactor.fetchSongsWasCalled, "HomeViewController should have called interactor to fetch songs after view is loaded.")
     }
     
+    func test_NumberOfSectionsInSongsListIs1() {
+        let tableView = homeViewController.tableView
+        let numberOfSection = homeViewController.numberOfSections(in: tableView)
+        let expectedNumberOfSection = 1
+        XCTAssertEqual(numberOfSection, expectedNumberOfSection, "number of sections in songs list should be 1.")
+    }
+    
+    func test_NumberOfRowsInSection0IsTheNumberOfSongsOfDisplaySongs() {
+        let tableView = homeViewController.tableView
+        let displaySong = Home.FetchSongs.ViewModel.DisplaySong(artistName: "", name: "", collectionName: "", artworkUrl100: "", artistUrl: "")
+        let displaySongs = [displaySong, displaySong, displaySong, displaySong]
+        homeViewController.displaySongs = displaySongs
+        
+        let numberOfRowsInSection0 = homeViewController.tableView(tableView, numberOfRowsInSection: 0)
+        let expectedNumberOfRowsInSection0 = displaySongs.count
+        XCTAssertEqual(numberOfRowsInSection0, expectedNumberOfRowsInSection0, "number of rows in section 0 should be equal to the number of display songs")
+    }
+    
+    func test_CellForRowsInSection0ShouldConfigureWithTheCorrespondingDisplaySong() {
+        let tableView = homeViewController.tableView
+        let displaySong = Home.FetchSongs.ViewModel.DisplaySong(artistName: "artistName", name: "name", collectionName: "collectionName", artworkUrl100: "", artistUrl: "")
+        let displaySongs = [displaySong]
+        homeViewController.displaySongs = displaySongs
+        
+        loadView()
+        
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as! SongTableViewCell
+        
+        XCTAssertEqual(cell.albumNameLabel.text, "collectionName")
+        XCTAssertEqual(cell.artistNameLabel.text, "artistName")
+    }
+    
+    func test_HeightForRowShouldReturn80() {
+        let tableView = homeViewController.tableView
+        let displaySong = Home.FetchSongs.ViewModel.DisplaySong(artistName: "", name: "", collectionName: "", artworkUrl100: "", artistUrl: "")
+        let displaySongs = [displaySong, displaySong, displaySong, displaySong]
+        homeViewController.displaySongs = displaySongs
+        
+        var heights: [CGFloat] = []
+        for i in 0..<displaySongs.count {
+            heights.append(homeViewController.tableView(tableView, heightForRowAt: IndexPath(row: i, section: 0)))
+        }
+        
+        let expectedHeights: [CGFloat] = [80, 80, 80, 80]
+        XCTAssertEqual(heights, expectedHeights, "Height of each row should be equal to 80")
+    }
+    
+    func test_SelectARowShouldDeselectItAutomatically() {
+        let tableView = homeViewController.tableView
+        let displaySong = Home.FetchSongs.ViewModel.DisplaySong(artistName: "", name: "", collectionName: "", artworkUrl100: "", artistUrl: "")
+        let displaySongs = [displaySong, displaySong, displaySong, displaySong]
+        homeViewController.displaySongs = displaySongs
+        
+        loadView()
+        
+        homeViewController.tableView(tableView, didSelectRowAt: IndexPath(row: 0, section: 0))
+        XCTAssertFalse(tableView.cellForRow(at: IndexPath(row: 0, section: 0))!.isSelected)
+    }
+    
     class HomeConfiguratorSpy: HomeConfigurationDelegate {
         var interactor: HomeBusinessLogicSpy!
         
